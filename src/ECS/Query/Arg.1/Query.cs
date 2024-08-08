970d3cd13301350d3e5af4481ff01d47d7b8f8a1
Ullrich Praetz <ullrich@praetz.de>
2024-08-08 07:56:46 +0200

    ECS - removed unused imports

--- src/ECS/Query/Arg.1/Query.cs
+++ src/ECS/Query/Arg.1/Query.cs
@@ -2,9 +2,6 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
-using static System.Diagnostics.DebuggerBrowsableState;
-using Browse = System.Diagnostics.DebuggerBrowsableAttribute;
-using static Friflo.Engine.ECS.StructInfo;
 
 // ReSharper disable once CheckNamespace
 namespace Friflo.Engine.ECS;
