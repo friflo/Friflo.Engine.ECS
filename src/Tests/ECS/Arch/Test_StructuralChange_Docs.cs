0332cdde66d842e3c7e1fa968b0a23e54c15462b
friflo <ullrich@praetz.de>
2025-02-13 13:34:41 +0100

    Test_StructuralChange_Docs - wip

--- src/Tests/ECS/Arch/Test_StructuralChange_Docs.cs
+++ src/Tests/ECS/Arch/Test_StructuralChange_Docs.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Friflo.Engine.ECS;
 using NUnit.Framework;
 
 
@@ -8,6 +9,7 @@ using NUnit.Framework;
 // ReSharper disable InconsistentNaming
 namespace Tests.ECS.Arch {
 
+[Ignore("source for docs: https://friflo.gitbook.io/friflo.engine.ecs/documentation/query#structuralchangeexception")]
 public static class Test_StructuralChange_Docs
 {
 [Test]
@@ -15,10 +17,32 @@ public static void CollectionModifiedException()
 {
     var list = new List<int> { 1, 2, 3 };
     foreach (var item in list) {
-        list.Add(42); // throws InvalidOperationException : Collection was modified; enumeration operation may not execute.
+        // throws InvalidOperationException : Collection was modified; enumeration operation may not execute.
+        list.Add(42);
     }
 }
+
+
+[Test]
+public static void QueryException()
+{
+    var store = new EntityStore();
+    store.CreateEntity(new Position());
+
+    var query = store.Query<Position>();
+    query.ForEachEntity((ref Position position, Entity entity) =>
+    {
+        // throws StructuralChangeException: within query loop. See: https://friflo.gitbook.io/friflo.engine.ecs/documentation/query#structuralchangeexception
+        entity.Add(new EntityName("test"));
+    });
     
+    // Valid approach using a CommandBuffer 
+    var buffer = store.GetCommandBuffer();
+    query.ForEachEntity((ref Position position, Entity entity) => {
+        buffer.AddComponent(entity.Id, new EntityName("test"));
+    });
+    buffer.Playback();
+}
 }
 
 }
