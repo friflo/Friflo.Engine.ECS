b057c10ae430fb0c1a7b1feef2c62e7410f818cf
friflo <ullrich@praetz.de>
2025-05-25 14:46:21 +0200

    Test_Asset - wip

--- src/Tests/ECS/Runtime/Test_Asset.cs
+++ src/Tests/ECS/Runtime/Test_Asset.cs
@@ -19,17 +19,30 @@ namespace Tests.ECS.Relations {
 
 public static class Test_Asset
 {
-    private class StringLoader : IAssetLoader<string>
+    private class StringLoader : IAssetLoader<string>, IInstanceFactory<string>
     {
         public string LoadAsset(string path, AssetSchema assetSchema) {
             return File.ReadAllText(path);
+        }
+        
+        public string CreateInstance(string path, out AbstractAsset asset) {
+            var typedAsset = Asset<string>.Get(path);
+            asset = typedAsset;
+            return typedAsset?.Resource;
+        }
+    
+        public AbstractAsset GetAsset (string path) {
+            return Asset<string>.Get(path);
         }
     }
     
     private static void RegisterAssetLoaders() {
         var folder = Directory.GetCurrentDirectory() + "../../../../";
         AssetDatabase.SetAssetFolder(folder);
-        AssetDatabase.RegisterAssetLoader(new StringLoader());
+        
+        var stringLoader = new StringLoader();
+        AssetDatabase.RegisterAssetLoader(stringLoader);
+        AssetDatabase.RegisterInstanceFactory(stringLoader);
     }
     
     [Test]
@@ -40,6 +53,7 @@ public static class Test_Asset
         AreEqual("Hello Asset!",                    asset.Resource);
         AreEqual("res://assets/string_asset.txt",   asset.path);
         AreEqual(AssetSchema.RES,                   asset.schema);
+        AreEqual("res://assets/string_asset.txt",   asset.ToString());
     }
     
     [Test]
@@ -49,10 +63,11 @@ public static class Test_Asset
         {
             var asset = Asset<string>.Get("xxx://assets/foo");
             IsNull(asset.Resource);
-            AreEqual("xxx://assets/foo",        asset.path);
-            AreEqual(AssetSchema.NONE,          asset.schema);
-            AreEqual(AssetError.INVALID_SCHEMA, asset.Error);
-            AreEqual("IS",                      AbstractAsset.GetErrorCode(asset));
+            AreEqual("xxx://assets/foo",                                    asset.path);
+            AreEqual(AssetSchema.NONE,                                      asset.schema);
+            AreEqual(AssetError.INVALID_SCHEMA,                             asset.Error);
+            AreEqual("IS",                                                  AbstractAsset.GetErrorCode(asset));
+            AreEqual("xxx://assets/foo - Missing or unknown asset type",    asset.ToString());
         } {
             var asset = Asset<string>.Get("res://not_found");
             IsNull(asset.Resource);
@@ -60,7 +75,18 @@ public static class Test_Asset
             AreEqual(AssetSchema.RES,           asset.schema);
             AreEqual(AssetError.FILE_NOT_FOUND, asset.Error);
             AreEqual("FNF",                     AbstractAsset.GetErrorCode(asset));
+            AreEqual("res://not_found",         asset.ToString());
         }
+    }
+    
+    [Test]
+    public static void Test_Asset_CreateInstance()
+    {
+        RegisterAssetLoaders();
+        var handle = Handle<string>.CreateInstance("res://assets/string_asset.txt");
+        AreEqual("Hello Asset!",                    handle.instance);
+        AreEqual("res://assets/string_asset.txt",   handle.path);
+        AreEqual("res://assets/string_asset.txt",   handle.ToString());
     }
 }
 
