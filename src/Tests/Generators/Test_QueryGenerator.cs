ef927e7ccb91c865a991fbd17e55f33ed8b5bc37
friflo <ullrich@praetz.de>
2026-03-18 12:54:17 +0100

    query gen - wip

--- src/Tests/Generators/Test_QueryGenerator.cs
+++ src/Tests/Generators/Test_QueryGenerator.cs
@@ -1,6 +1,7 @@
 using Friflo.Engine.ECS;
 using NUnit.Framework;
 using Tests.ECS;
+using static NUnit.Framework.Assert;
 
 // ReSharper disable CheckNamespace
 namespace GeneratedCode;
@@ -9,10 +10,10 @@ namespace GeneratedCode;
 public partial class MyExample
 {
     [Query] // This triggers the generator
-    void MoveExample(ref Position position) { }
+    void MoveExample(ref Position position) { position.x = 1; }
     
     [Query] // This triggers the generator
-    void MoveExample2(ref MyComponent1 myComponent1, int deltaTime) { } 
+    static void MoveExample2(ref MyComponent1 myComponent1, int someValue) { myComponent1.a = someValue; } 
 }
 
 public static class Test_QueryGenerator
@@ -22,6 +23,12 @@ public static class Test_QueryGenerator
     {
         var tester = new MyExample();
         var store = new EntityStore();
+        var entity = store.CreateEntity(new Position(), new MyComponent1());
+        
         tester.MoveExampleQuery(store);
+        AreEqual(1, entity.GetComponent<Position>().x);
+        
+        MyExample.MoveExample2Query(store, 123);
+        AreEqual(123, entity.GetComponent<MyComponent1>().a);
     }
 }
