03836cf55fd2bd74c9b6312fbe0d3a032f371048
friflo <ullrich@praetz.de>
2026-03-28 07:38:21 +0100

    vectorize - wip

--- src/ECS.Generators/Types.cs
+++ src/ECS.Generators/Types.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
