c8c19ae0a700b200ca7092486151f0f72a12569f
friflo <ullrich@praetz.de>
2026-03-28 13:34:15 +0100

    vectorize - MultiplyAdd verify

--- src/Tests-Generators/Generators/Vectorize/Verify_Vectorize.cs
+++ src/Tests-Generators/Generators/Vectorize/Verify_Vectorize.cs
@@ -104,4 +104,36 @@ public partial class MyExample
         // 4. Verify (NUnit adapter)
         await Verifier.Verify(runResult);
     }
+    
+    [Test]
+    public static async Task  Verify_Query_MultiplyAdd()
+    {
+        // 1. Your Input Source
+        var code =
+            """
+            using Friflo.Engine.ECS;
+
+            namespace VerifyVectorize;
+
+            public struct Velocity : IComponent { public Vector3 value; }
+
+            public partial class MyExample
+            {
+                [Vectorize][Query]  [OmitHash]
+                void AssignVector(ref Position position, in Velocity velocity, float deltaTime) {
+                    position.value += velocity.value * deltaTime;
+                }
+            }
+            """;
+        // 2. Setup (Helper method suggested for readability)
+        var compilation = VerifyUtils.CreateCompilation(code);
+        var generator = new AttributeQueryGenerator();
+        var driver = CSharpGeneratorDriver.Create(generator);
+
+        // 3. Run
+        var runResult = driver.RunGenerators(compilation);
+
+        // 4. Verify (NUnit adapter)
+        await Verifier.Verify(runResult);
+    }
 }
