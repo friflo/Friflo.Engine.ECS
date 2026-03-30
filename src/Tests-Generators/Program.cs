0a7e2ae9473042ea476b07eb1c4f5a6fc7e3bcef
friflo <ullrich@praetz.de>
2026-03-30 12:29:00 +0200

    vectorize - bench

--- src/Tests-Generators/Program.cs
+++ src/Tests-Generators/Program.cs
@@ -1,3 +1,10 @@
+using System.Reflection;
 using BenchmarkDotNet.Running;
 
-BenchmarkRunner.Run<Bench_Vector3>();
+// BenchmarkRunner.Run<Bench_Vector3>();
+BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
+
+// CLI examples
+//      dotnet run -c Release
+//      dotnet run -c Release --filter *Vector3*
+//      dotnet run -c Release --filter *Vector3_MultiplyAdd*
