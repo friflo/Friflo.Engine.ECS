acc22db983c48329623eaf59cdb30e4395b9ca23
friflo <ullrich@praetz.de>
2026-03-30 16:33:35 +0200

    vectorize - add Bench_Vector2

---
+++ src/Tests-Generators/Generators/Benchmark/Bench_Vector2.cs
@@ -0,0 +1,65 @@
+using System.Numerics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using Friflo.Engine.ECS;
+using Tests.ECS;
+using Tests.Examples;
+using Tests.Generators.Benchmark;
+
+
+[MemoryDiagnoser] // Tracks GC allocations
+// [Config(typeof(Config))]
+public partial class Bench_Vector2
+{
+    private class Config : ManualConfig
+    {
+        public Config() 
+        {
+            // Add the standard columns + our new Comment column
+            AddColumn(new CommentColumn()); 
+        }
+    }
+    
+    private EntityStore store;
+    private ArchetypeQuery<Position2,Velocity2> query;
+
+    const int EntityCount = 1000;
+    
+    [Vectorize][Query]  [OmitHash]
+    private static void MultiplyAdd(ref Position2 position, ref Velocity2 velocity, float deltaTime) {
+        position.value = velocity.value * deltaTime + position.value;
+    }
+
+    [GlobalSetup]
+    public void Setup() {
+        store = new EntityStore();
+        for (int n = 0; n < EntityCount; n++) {
+            store.CreateEntity(new Position2 { value = new Vector2(n,n)}, new Velocity2 { value = new Vector2(1,2)}, new FloatComponent { value = n });
+        }
+        query = store.Query<Position2, Velocity2>();
+    }
+
+    [Benchmark]
+    [Comment("pos.value = vel.value * dt + pos.value;")]
+    public void Vector3_MultiplyAdd_Query()
+    {
+        MultiplyAddQuery(store, 0.1f, false);
+    }
+
+    [Benchmark]
+    [Comment("pos.value = vel.value * dt + pos.value;")]
+    public void Vector3_MultiplyAdd_Vectorize()
+    {
+        MultiplyAddQuery(store, 0.1f);
+    }
+    
+    [Benchmark]
+    [Comment("pos.value = vel.value * dt + pos.value;")]
+    public void Vector3_MultiplyAdd_ForEachEntity()
+    {
+        var deltaTime = 0.1f;
+        query.ForEachEntity((ref Position2 position, ref Velocity2 velocity, Entity entity) => {
+            position.value = velocity.value * deltaTime + position.value;
+        });
+    }
+}
