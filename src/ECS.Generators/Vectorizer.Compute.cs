4410d80b4405fe428837803846f56ef34175a084
friflo <ullrich@praetz.de>
2026-04-01 12:00:44 +0200

    vectorize

--- src/ECS.Generators/Vectorizer.Compute.cs
+++ src/ECS.Generators/Vectorizer.Compute.cs
@@ -6,6 +6,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
+// ReSharper disable ForCanBeConvertedToForeach
 namespace Friflo.Engine.ECS.Generators;
 
 public static partial class Vectorizer
