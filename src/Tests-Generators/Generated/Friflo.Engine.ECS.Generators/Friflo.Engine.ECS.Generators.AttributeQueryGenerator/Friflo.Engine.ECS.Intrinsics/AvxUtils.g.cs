d1aa3486d858b06dbc2f9767acd10b8471909e07
friflo <ullrich@praetz.de>
2026-04-02 14:47:36 +0200

    vectorize

--- src/Tests-Generators/Generated/Friflo.Engine.ECS.Generators/Friflo.Engine.ECS.Generators.AttributeQueryGenerator/Friflo.Engine.ECS.Intrinsics/AvxUtils.g.cs
+++ src/Tests-Generators/Generated/Friflo.Engine.ECS.Generators/Friflo.Engine.ECS.Generators.AttributeQueryGenerator/Friflo.Engine.ECS.Intrinsics/AvxUtils.g.cs
@@ -25,6 +25,7 @@ namespace Friflo.Engine.ECS.Intrinsics
 
     internal static class MathUtils
     {
+        // ------ Trigonometry
         [SkipLocalsInit]
         internal static Vector256<float> SinMathF(Vector256<float> x)
         {
@@ -66,11 +67,78 @@ namespace Friflo.Engine.ECS.Intrinsics
         {
             return Vector256.Create(Atan2(y[0],x[0]), Atan2(y[1],x[1]), Atan2(y[2],x[2]), Atan2(y[3],x[3]), Atan2(y[4],x[4]), Atan2(y[5],x[5]), Atan2(y[6],x[6]), Atan2(y[7],x[7]));
         }
-
+        
+        [SkipLocalsInit]
+        internal static Vector256<float> AsinhMathF(Vector256<float> x)
+        {
+            return Vector256.Create(Asinh(x[0]), Asinh(x[1]), Asinh(x[2]), Asinh(x[3]), Asinh(x[4]), Asinh(x[5]), Asinh(x[6]), Asinh(x[7]));
+        }
+        
+        [SkipLocalsInit]
+        internal static Vector256<float> AcoshMathF(Vector256<float> x)
+        {
+            return Vector256.Create(Acosh(x[0]), Acosh(x[1]), Acosh(x[2]), Acosh(x[3]), Acosh(x[4]), Acosh(x[5]), Acosh(x[6]), Acosh(x[7]));
+        }
+        
+        [SkipLocalsInit]
+        internal static Vector256<float> AtanhMathF(Vector256<float> x)
+        {
+            return Vector256.Create(Atanh(x[0]), Atanh(x[1]), Atanh(x[2]), Atanh(x[3]), Atanh(x[4]), Atanh(x[5]), Atanh(x[6]), Atanh(x[7]));
+        }
+        
+        // ------ misc
         [SkipLocalsInit]
         internal static Vector256<float> TruncateMathF(Vector256<float> x)
         {
             return Vector256.Create(Truncate(x[0]), Truncate(x[1]), Truncate(x[2]), Truncate(x[3]), Truncate(x[4]), Truncate(x[5]), Truncate(x[6]), Truncate(x[7]));
+        }
+        
+        [SkipLocalsInit]
+        internal static Vector256<float> FloorMathF(Vector256<float> x)
+        {
+            return Vector256.Create(Floor(x[0]), Floor(x[1]), Floor(x[2]), Floor(x[3]), Floor(x[4]), Floor(x[5]), Floor(x[6]), Floor(x[7]));
+        }
+        
+        [SkipLocalsInit]
+        internal static Vector256<float> ExpMathF(Vector256<float> x)
+        {
+            return Vector256.Create(Exp(x[0]), Exp(x[1]), Exp(x[2]), Exp(x[3]), Exp(x[4]), Exp(x[5]), Exp(x[6]), Exp(x[7]));
+        }
+        
+        [SkipLocalsInit]
+        internal static Vector256<float> LogMathF(Vector256<float> x)
+        {
+            return Vector256.Create(Log(x[0]), Log(x[1]), Log(x[2]), Log(x[3]), Log(x[4]), Log(x[5]), Log(x[6]), Log(x[7]));
+        }
+        
+        [SkipLocalsInit]
+        internal static Vector256<float> Log10MathF(Vector256<float> x)
+        {
+            return Vector256.Create(Log10(x[0]), Log10(x[1]), Log10(x[2]), Log10(x[3]), Log10(x[4]), Log10(x[5]), Log10(x[6]), Log10(x[7]));
+        }
+        
+        [SkipLocalsInit]
+        internal static Vector256<float> Log2MathF(Vector256<float> x)
+        {
+            return Vector256.Create(Log2(x[0]), Log2(x[1]), Log2(x[2]), Log2(x[3]), Log2(x[4]), Log2(x[5]), Log2(x[6]), Log2(x[7]));
+        }
+        
+        [SkipLocalsInit]
+        internal static Vector256<float> PowMathF(Vector256<float> x, Vector256<float> y)
+        {
+            return Vector256.Create(Pow(x[0],y[0]), Atan2(x[1],y[1]), Atan2(x[2],y[2]), Atan2(x[3],y[3]), Atan2(x[4],y[4]), Atan2(x[5],y[5]), Atan2(x[6],y[6]), Atan2(x[7],y[7]));
+        }
+        
+        [SkipLocalsInit]
+        internal static Vector256<float> RoundMathF(Vector256<float> x)
+        {
+            return Vector256.Create(Round(x[0]), Round(x[1]), Round(x[2]), Round(x[3]), Round(x[4]), Round(x[5]), Round(x[6]), Round(x[7]));
+        }
+        
+        [SkipLocalsInit]
+        internal static Vector256<float> SqrtMathF(Vector256<float> x)
+        {
+            return Vector256.Create(Sqrt(x[0]), Sqrt(x[1]), Sqrt(x[2]), Sqrt(x[3]), Sqrt(x[4]), Sqrt(x[5]), Sqrt(x[6]), Sqrt(x[7]));
         }
     }
 }
