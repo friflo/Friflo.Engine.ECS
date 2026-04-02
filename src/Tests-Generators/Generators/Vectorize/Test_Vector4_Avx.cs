443974bc73a61cca39b525ae4a581cdcf10e0af4
friflo <ullrich@praetz.de>
2026-04-02 17:45:29 +0200

    vectorize - support const literal in Vector4

--- src/Tests-Generators/Generators/Vectorize/Test_Vector4_Avx.cs
+++ src/Tests-Generators/Generators/Vectorize/Test_Vector4_Avx.cs
@@ -247,6 +247,30 @@ public static partial class Test_Vector4_Avx
     
     // -----------------------------------------------------------------------------------------------------
     [Vectorize][Query]  [OmitHash]
+    private static void Multiply_Vector4_Const(ref Position4 position, ref Velocity4 velocity)
+    {
+        position.value = velocity.value * 2;
+    }
+
+    [Test]
+    public static void Test_Multiply_Vector4_Const()
+    {
+        var store = CreateTestStore();
+        Multiply_Vector4_ConstQuery(store, false);
+
+        var storeVectorized = CreateTestStore();
+        var query = Multiply_Vector4_ConstQuery(storeVectorized);
+
+        Assert.That(query.Count, Is.EqualTo(EntityCount));
+        foreach (var entity in store.Entities)
+        {
+            var entityVectorized = storeVectorized.GetEntityById(entity.Id);
+            Assert.That(entity.GetComponent<Position4>(), Is.EqualTo(entityVectorized.GetComponent<Position4>()));
+        }
+    }
+    
+    // -----------------------------------------------------------------------------------------------------
+    [Vectorize][Query]  [OmitHash]
     private static void Multiply_Vector4_Matrix4x4(ref Position4 position, in Matrix4x4 matrix) {
         position.value = Vector4.Transform(position.value, matrix);
     }
