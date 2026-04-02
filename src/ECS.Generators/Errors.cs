652189c80d1ea0809a2a0976d63f6870a8d01c7f
friflo <ullrich@praetz.de>
2026-04-02 15:40:00 +0200

    vectorize

--- src/ECS.Generators/Errors.cs
+++ src/ECS.Generators/Errors.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
 using Microsoft.CodeAnalysis;
 
 namespace Friflo.Engine.ECS.Generators;
