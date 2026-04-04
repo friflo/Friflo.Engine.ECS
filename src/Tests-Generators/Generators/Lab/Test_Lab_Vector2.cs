e49aae77db67c841f6d8680c566341a6f0506add
friflo <ullrich@praetz.de>
2026-04-04 15:24:31 +0200

    vectorize

---
+++ src/Tests-Generators/Generators/Lab/Test_Lab_Vector2.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Ullrich Praetz - https://github.com/friflo. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Numerics;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming
+namespace Tests.Generators.Lab;
+
+
+public static class Test_Lab_Vector2
+{
+    // ------------------------------- scalar component -------------------------------
+    [Test]
+    public static unsafe void Test_Lab_Vector2_Interleave()
+    {
+        var vectors = new Vector2[]
+        {
+            new(11, 12),
+            new(21, 22),
+            new(31, 32),
+            new(41, 42),
+            new(51, 52),
+            new(61, 62),
+            new(71, 72),
+            new(81, 82),
+            
+            new( 91, 92),
+            new(101,102),
+            new(111,112),
+            new(121,122),
+            new(131,132),
+            new(141,142),
+            new(151,152),
+            new(161,162),
+        };
+        var expectX1 = Vector256.Create(new float [] { 11,  21,  31,  41,  51,  61,  71,  81 });
+        var expectX2 = Vector256.Create(new float [] { 91, 101, 111, 121, 131, 141, 151, 161 });
+        var expectY1 = Vector256.Create(new float [] { 12,  22,  32,  42,  52,  62,  72,  82 });
+        var expectY2 = Vector256.Create(new float [] { 92, 102, 112, 122, 132, 142, 152, 162 });
+
+        fixed (Vector2* vectors_ptr = vectors)
+        {
+            var ptr = (float*)vectors_ptr;
+            // 1. Load 24 floats into three 256-bit registers
+            Vector256<float> v0 = Avx.LoadVector256(ptr);      // [X0 Y0 X1 Y1 X2 Y2 X3 Y3]
+            Vector256<float> v1 = Avx.LoadVector256(ptr + 8);  // [X4 Y4 X5 Y5 X6 Y6 X7 Y7]
+            Vector256<float> v2 = Avx.LoadVector256(ptr + 16);
+            Vector256<float> v3 = Avx.LoadVector256(ptr + 24);
+
+            var (x1, y1) = AvxVector2.Deinterleave(v0, v1);
+            var (x2, y2) = AvxVector2.Deinterleave(v2, v3);
+            
+            Assert.That(x1, Is.EqualTo(expectX1));
+            Assert.That(y1, Is.EqualTo(expectY1));
+            Assert.That(x2, Is.EqualTo(expectX2));
+            Assert.That(y2, Is.EqualTo(expectY2));
+            
+            var (res0, res1) = AvxVector2.Interleave(x1,y1);
+            Assert.That(res0, Is.EqualTo(v0));
+            Assert.That(res1, Is.EqualTo(v1));
+            
+            var (res2, res3) = AvxVector2.Interleave(x2,y2);
+            Assert.That(res2, Is.EqualTo(v2));
+            Assert.That(res3, Is.EqualTo(v3));
+        }
+    }
+}
+
+
+
--- src/Tests-Generators/Generators/Lab/Test_Lab_Vector3.cs
+++ src/Tests-Generators/Generators/Lab/Test_Lab_Vector3.cs
@@ -2,7 +2,6 @@
 // See LICENSE file in the project root for full license information.
 
 using System.Numerics;
-using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.X86;
 using NUnit.Framework;
@@ -15,7 +14,7 @@ public static class Test_Lab_Vector3
 {
     // ------------------------------- scalar component -------------------------------
     [Test]
-    public static unsafe void Test_Lab_Vector3_Transpose()
+    public static unsafe void Test_Lab_Vector3_Interleave()
     {
         var vectors = new Vector3[]
         {
@@ -40,7 +39,7 @@ public static class Test_Lab_Vector3
             Vector256<float> v1 = Avx.LoadVector256(ptr + 8); // [Z2 X3 Y3 Z3 X4 Y4 Z4 X5]
             Vector256<float> v2 = Avx.LoadVector256(ptr + 16); // [Y5 Z5 X6 Y6 Z6 X7 Y7 Z7]
 
-            var (x1, y1, z1) = Approach1.Deinterleave(v0, v1, v2);
+            var (x1, y1, z1) = AvxVector3.Deinterleave(v0, v1, v2);
             var (x2, y2, z2) = Approach2.Deinterleave(v0, v1, v2);
             var (x3, y3, z3) = Approach3.DeinterleaveExtreme(v0, v1, v2);
             var (x4, y4, z4) = Approach4.Deinterleave(v0,v1,v2);
@@ -49,64 +48,11 @@ public static class Test_Lab_Vector3
             Assert.That(y1, Is.EqualTo(expect2));
             Assert.That(z1, Is.EqualTo(expect3));
             
-            var (res1, res2, res3) = Approach4.Interleave(x1,y1,z1);
+            var (res1, res2, res3) = AvxVector3.Interleave(x1,y1,z1);
             Assert.That(res1, Is.EqualTo(v0));
             Assert.That(res2, Is.EqualTo(v1));
             Assert.That(res3, Is.EqualTo(v2));
         }
-    }
-}
-
-public static class Approach1
-{
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static   (Vector256<float>  X, Vector256<float>  Y, Vector256<float>  Z)
-        Deinterleave(Vector256<float> v0, Vector256<float> v1, Vector256<float> v2)
-    {
-        // --- STEP 1: Lane-Level Alignment ---
-        // We use Shuffle (VSHUFPS) to move Zs out of the way. 
-        // This is a 1-cycle latency instruction.
-        // Result: [X0 Y0 X1 Y1 | X2 Y2 ?? ??] 
-        // var x0y0x1y1 = Avx.Shuffle(v0, v0, 0b01_00_01_00);
-
-        // --- STEP 2: The "Magic" Permutes ---
-        // Instead of many blends, we use exactly 3 PermuteVar8x32 calls.
-        // On modern CPUs, the 3-cycle latency of Permute is hidden 
-        // by the sheer throughput of the rest of the pipeline.
-
-        // Masks must be static readonly for maximum performance
-        // X indices: 0, 3, 6 (v0), 9, 12, 15 (v1), 18, 21 (v2) -> relative to base
-        // Because we process in 256-bit chunks, we use these specific patterns:
-        var x       = Avx2.PermuteVar8x32(v0, Vector256.Create(0, 3, 6, 0, 0, 0, 0, 0));
-        var x_mid   = Avx2.PermuteVar8x32(v1, Vector256.Create(0, 0, 0, 1, 4, 7, 0, 0));
-        var x_end   = Avx2.PermuteVar8x32(v2, Vector256.Create(0, 0, 0, 0, 0, 0, 2, 5));
-
-        // Combine using the fastest Blend instruction (VBLENDPS)
-        x = Avx.Blend(x, x_mid, 0b00111000); // 0x38
-        x = Avx.Blend(x, x_end, 0b11000000); // 0xC0
-
-        // Y indices: 1, 4, 7 (v0), 10, 13, 16 (v1), 19, 22 (v2)
-        var y           = Avx2.PermuteVar8x32(v0, Vector256.Create(1, 4, 7, 0, 0, 0, 0, 0));
-        var y_mid       = Avx2.PermuteVar8x32(v1, Vector256.Create(0, 0, 0, 2, 5, 0, 0, 0));
-        var y_end_raw   = Avx2.PermuteVar8x32(v2, Vector256.Create(0, 0, 0, 0, 0, 0, 3, 6));
-
-        y = Avx.Blend(y, y_mid, 0b00011000); // 0x18
-        // Y5 is index 0 of v2, so we blend it manually
-        var y5 = Avx2.PermuteVar8x32(v2, Vector256.Create(0, 0, 0, 0, 0, 0, 0, 0));
-        y = Avx.Blend(y,        y5, 0b00100000); // 0x20
-        y = Avx.Blend(y, y_end_raw, 0b11000000);
-
-        // Z indices: 2, 5, 8 (v1-index0), 11, 14, 17 (v2), 20, 23
-        var z       = Avx2.PermuteVar8x32(v0, Vector256.Create(2, 5, 0, 0, 0, 0, 0, 0));
-        var z2      = Avx2.PermuteVar8x32(v1, Vector256.Create(0, 0, 0, 0, 0, 0, 0, 0)); // Z2
-        var z_mid   = Avx2.PermuteVar8x32(v1, Vector256.Create(0, 0, 0, 3, 6, 0, 0, 0));
-        var z_end   = Avx2.PermuteVar8x32(v2, Vector256.Create(0, 0, 0, 0, 0, 1, 4, 7));
-
-        z = Avx.Blend(z,    z2, 0b00000100); // 0x04
-        z = Avx.Blend(z, z_mid, 0b00011000); // 0x18
-        z = Avx.Blend(z, z_end, 0b11100000); // 0xE0
-
-        return (x, y, z);
     }
 }
 
@@ -323,41 +269,7 @@ public static class Approach4
         return (res0, res1, res2);
     }
     
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static (Vector256<float> V0, Vector256<float> V1, Vector256<float> V2) Interleave(
-        Vector256<float> vx, Vector256<float> vy, Vector256<float> vz)
-    {
-        // 1. Initial Unpacks (Very fast, Port 5)
-        var lowXY  = Avx.UnpackLow(vx, vy);   
-        var highXY = Avx.UnpackHigh(vx, vy); 
-        
-        // 2. Fix Lanes (Required for 3-way stride)
-        var xy_0123 = Avx.Permute2x128(lowXY, highXY, 0x20);
-        var xy_4567 = Avx.Permute2x128(lowXY, highXY, 0x31);
 
-        // 3. V0: Use immediate Blend instead of BlendVariable
-        // Mask 0b00100100 (hex 0x24) targets indices 2 and 5
-        var res0 = Avx2.PermuteVar8x32(xy_0123, Vector256.Create(0, 1, 0, 2, 3, 0, 4, 5));
-        var z_v0 = Avx2.PermuteVar8x32(vz,      Vector256.Create(0, 0, 0, 0, 0, 1, 0, 0).AsInt32()); // Z0, Z1
-        var v0 = Avx.Blend(res0, z_v0, 0x24); 
-
-        // 4. V1: Optimized Bridge
-        var v1_xyL = Avx2.PermuteVar8x32(xy_0123, Vector256.Create(0, 6, 7, 0, 0, 0, 0, 0));
-        var v1_xyH = Avx2.PermuteVar8x32(xy_4567, Vector256.Create(0, 0, 0, 0, 0, 1, 0, 2));
-        var v1_z   = Avx2.PermuteVar8x32(vz,      Vector256.Create(2, 0, 0, 3, 0, 0, 4, 0));
-        
-        // Combine XY parts with an immediate blend (Port 0/5)
-        var v1_tmp = Avx.Blend(v1_xyL, v1_xyH, 0xB0); // 0b10110000
-        // Final Z merge
-        var v1 = Avx.Blend(v1_tmp, v1_z, 0x49); // 0b01001001 (Indices 0, 3, 6)
-
-        // 5. V2: Optimized Tail
-        var res2 = Avx2.PermuteVar8x32(xy_4567, Vector256.Create(3, 0, 4, 5, 0, 6, 7, 0));
-        var z_v2 = Avx2.PermuteVar8x32(vz,      Vector256.Create(0, 5, 0, 0, 6, 0, 0, 7));
-        var v2 = Avx.Blend(res2, z_v2, 0x92); // 0b10010010 (Indices 1, 4, 7)
-
-        return (v0, v1, v2);
-    }
 }
 
 
